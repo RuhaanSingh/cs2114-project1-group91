f3ca51cd49ed0adc023a6a03a5585508dc0f0c24
ajeetbvt <ajeetb@vt.edu>
2026-09-23 22:14:47 -0400

    Fix class name mismatch in BenjaminMaloney.java placeholder (was 'YourName')

--- BenjaminMaloney.java
+++ BenjaminMaloney.java
@@ -1,5 +1,5 @@
-public class YourName {
+public class BenjaminMaloney {
     public static void main(String[] args) {
-        System.out.println("Your Name is on the team.");
+        System.out.println("Benjamin Maloney is on the team.");
     }
 }
